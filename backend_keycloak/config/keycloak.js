5d9630992906342ee242216c7e4c9555018acbcb
Fouad BANAN <bananfouad9@gmail.com>
2026-05-09 02:52:35 +0200

    clean the initial setup and configure keycloak props

--- backend_keycloak/config/keycloak.js
+++ backend_keycloak/config/keycloak.js
@@ -5,10 +5,10 @@ import Keycloak from 'keycloak-connect';
 const memoryStore = new session.MemoryStore();
 
 const keycloakConfig = {
-  clientId: 'myClient',
+  clientId: 'IA_indus',
   bearerOnly: true, // if you only use Bearer tokens
   serverUrl: 'http://localhost:8081/',
-  realm: 'Realm1',
+  realm: 'IA_Indus',
 };
 
 const keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
